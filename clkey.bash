4ba9b28475e7379b4a1f24d3abc9495fe9ed5c6f
bronson <bronson@cygnus.lan>
2025-09-03 10:30:00 -0500

    Added documentation

--- clkey.bash
+++ clkey.bash
@@ -1,3 +1,8 @@
+# clkey()
+# With a numeric argument n, this command deletes line n from known_hosts
+# Without an argument, this command clears all hosts from known_hosts
+# A backup of the .previous version is created at runtime
+# Only the last .previous version is retained
 clkey()
 {
 	sed -i .previous "$1d" ~/.ssh/known_hosts
